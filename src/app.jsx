81e5e3551386f2e1422f8c397874d5aef32fc572
Klaudia <klaudia@tonik.pl>
2018-10-06 22:05:00 +0200

    create Photos list component

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
 import Select from './components/Select/Select';
+import PhotosList from './components/PhotosList/PhotosList';
 
 import './app.css';
 
@@ -14,12 +15,15 @@ const options = [
   { id: 4, name: 'random' },
 ];
 
+const photos = ['https://images.pexels.com/photos/426894/pexels-photo-426894.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'https://images.pexels.com/photos/571150/pexels-photo-571150.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', 'https://images.pexels.com/photos/1292684/pexels-photo-1292684.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'];
+
 const App = () => (
   <React.Fragment>
     <Button>Search</Button>
     <br />
     <Input type="number" />
     <Select head={head} options={options} />
+    <PhotosList photos={photos} />
   </React.Fragment>
 );
 
---
+++ src/components/PhotosList/PhotosList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { arrayOf, string } from 'prop-types';
+
+import './PhotosList.css';
+
+const PhotosList = ({ photos }) => (
+  <ul className="photos-list">
+    {photos.map(photo => (
+      <li key={photo} className="photos-list__item">
+        <div className="photos-list__photo" style={{ backgroundImage: `url(${photo})` }} />
+      </li>
+    ))}
+  </ul>
+);
+
+PhotosList.propTypes = {
+  photos: arrayOf(string).isRequired,
+};
+
+export default PhotosList;
