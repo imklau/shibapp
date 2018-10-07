9f04c1ad40c2d82e1ddd94ff477b1883567fe91b
Klaudia <klaudia@tonik.pl>
2018-10-07 15:07:04 +0200

    add button variants as prop; add onClick handler

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,22 +5,26 @@ import {
   node,
   number,
   string,
+  func,
 } from 'prop-types';
 
 import './Button.css';
 
 const Button = ({
   children,
+  type,
+  variant,
   loading,
   disabled,
-  type,
   size,
+  onClick,
 }) => (
   <button
-    disabled={disabled}
     type={type}
     loading={loading}
-    className={`button button--${size} ${loading ? 'button--loading' : ''}`}
+    disabled={disabled}
+    onClick={onClick || null}
+    className={`button button--${variant} button--${size} ${loading ? 'button--loading' : ''}`}
   >
     {loading ? 'loading' : children}
   </button>
@@ -28,17 +32,21 @@ const Button = ({
 
 Button.propTypes = {
   children: oneOfType([string, node, number]).isRequired,
+  type: string,
+  variant: string,
   loading: bool,
   disabled: bool,
-  type: string,
   size: string,
+  onClick: oneOfType([func, bool]),
 };
 
 Button.defaultProps = {
+  type: 'submit',
+  variant: 'primary',
   loading: false,
   disabled: false,
-  type: 'submit',
   size: 'regular',
+  onClick: false,
 };
 
 export default Button;
